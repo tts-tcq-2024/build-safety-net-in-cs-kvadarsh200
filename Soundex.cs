15113e18c8e05b64bcc2da74424b4af58925726e
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-05 23:47:49 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -14,22 +14,32 @@ public class Soundex
         soundex.Append(char.ToUpper(name[0]));
         char prevCode = GetSoundexCode(name[0]);
 
+        ProcessCharacters(name, soundex, ref prevCode);
+
+        return PadSoundex(soundex).ToString();
+    }
+
+    private static void ProcessCharacters(string name, StringBuilder soundex, ref char prevCode)
+    {
         for (int i = 1; i < name.Length && soundex.Length < 4; i++)
         {
             AppendSoundexCode(name[i], soundex, ref prevCode);
         }
-
-        return PadSoundex(soundex).ToString();
     }
 
     private static void AppendSoundexCode(char c, StringBuilder soundex, ref char prevCode)
     {
         char code = GetSoundexCode(c);
-        if (code != '0' && code != prevCode)
+        if (IsAppendableCode(code, prevCode))
         {
             soundex.Append(code);
             prevCode = code;
         }
+    }
+
+    private static bool IsAppendableCode(char code, char prevCode)
+    {
+        return code != '0' && code != prevCode;
     }
 
     private static StringBuilder PadSoundex(StringBuilder soundex)

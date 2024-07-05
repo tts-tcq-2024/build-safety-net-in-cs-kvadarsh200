e92342afebffa2b835a0a20ac4de6b8d35e87417
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-05 17:21:36 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Text;
+
 public class Soundex
 {
-    private static readonly int[] soundexMapping = new int[26] {
+    private static readonly int[] soundexMapping = new int[26]
+    {
         0, 1, 2, 3, 0, 1, 2, 0, 0, 2,
         2, 4, 5, 5, 0, 1, 2, 6, 2, 3,
         0, 1, 0, 2, 0, 2
@@ -16,21 +18,29 @@ public class Soundex
         }
 
         StringBuilder soundex = new StringBuilder();
-        soundex.Append(char.ToUpper(name[0]));
+        char firstLetter = char.ToUpper(name[0]);
+        soundex.Append(firstLetter);
+
         int sIndex = 1;
+        char previousCode = GetSoundexCode(firstLetter);
 
-        ProcessName(name, soundex, ref sIndex);
+        for (int i = 1; i < name.Length && sIndex < 4; i++)
+        {
+            char currentCode = GetSoundexCode(name[i]);
+            AppendSoundexCode(currentCode, ref previousCode, soundex, ref sIndex);
+        }
+
         FinalizeSoundex(soundex, sIndex);
-
         return soundex.ToString();
     }
 
-    private static void ProcessName(string name, StringBuilder soundex, ref int sIndex)
+    private static void AppendSoundexCode(char currentCode, ref char previousCode, StringBuilder soundex, ref int sIndex)
     {
-        for (int i = 1; i < name.Length && sIndex < 4; i++)
+        if (currentCode != '0' && currentCode != previousCode)
         {
-            char code = GetSoundexCode(name[i]);
-            AppendSoundexCode(code, soundex, ref sIndex);
+            soundex.Append(currentCode);
+            sIndex++;
+            previousCode = currentCode;
         }
     }
 
@@ -39,15 +49,6 @@ public class Soundex
         while (sIndex < 4)
         {
             soundex.Append('0');
-            sIndex++;
-        }
-    }
-
-    private static void AppendSoundexCode(char code, StringBuilder soundex, ref int sIndex)
-    {
-        if (code != '0' && (sIndex == 1 || code != soundex[sIndex - 1]))
-        {
-            soundex.Append(code);
             sIndex++;
         }
     }

3390e87e9d9f47363abbee3686604c2f909201d3
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-05 23:41:26 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -14,5 +14,44 @@ public class SoundexTests
         Assert.Equal("A000", Soundex.GenerateSoundex("A"));
     }
 
-   
+    [Fact]
+    public void HandlesMultipleCharacters()
+    {
+        Assert.Equal("A123", Soundex.GenerateSoundex("Aiden"));
+        Assert.Equal("B230", Soundex.GenerateSoundex("Babcock"));
+        Assert.Equal("C160", Soundex.GenerateSoundex("Christopher"));
+    }
+
+    [Fact]
+    public void HandlesMixedCase()
+    {
+        Assert.Equal("A123", Soundex.GenerateSoundex("aiden"));
+        Assert.Equal("B230", Soundex.GenerateSoundex("BaBcOcK"));
+        Assert.Equal("C160", Soundex.GenerateSoundex("CHRISTOPHER"));
+    }
+
+    [Fact]
+    public void HandlesRepeatingConsonants()
+    {
+        Assert.Equal("B120", Soundex.GenerateSoundex("Bobby"));
+    }
+
+    [Fact]
+    public void HandlesVowelsAndIgnoredCharacters()
+    {
+        Assert.Equal("R260", Soundex.GenerateSoundex("Robert"));
+        Assert.Equal("R163", Soundex.GenerateSoundex("Rupert"));
+    }
+
+    [Fact]
+    public void PadsWithZeros()
+    {
+        Assert.Equal("H000", Soundex.GenerateSoundex("H"));
+    }
+
+    [Fact]
+    public void TruncatesLongSoundex()
+    {
+        Assert.Equal("P120", Soundex.GenerateSoundex("Pfister"));
+    }
 }

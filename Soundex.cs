2a52a9b05add11206cba180f1fbbd9078b23772b
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-05 19:41:18 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,51 +1,54 @@
 using System;
 using System.Text;
 
-public static class Soundex
+public class Soundex
 {
-    private static readonly int[] SoundexMapping = {
+    private static readonly int[] soundexMapping = {
         0, 1, 2, 3, 0, 1, 2, 0, 0, 2,
         2, 4, 5, 5, 0, 1, 2, 6, 2, 3,
         0, 1, 0, 2, 0, 2
     };
 
-    private static char GetSoundexCode(char c)
+    public static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
         if (c < 'A' || c > 'Z')
         {
             return '0';
         }
-        return (char)(SoundexMapping[c - 'A'] + '0');
+        return (char)(soundexMapping[c - 'A'] + '0');
+    }
+
+    private static void AppendSoundexCode(char code, StringBuilder soundex)
+    {
+        if (code != '0' && (soundex.Length == 0 || code != soundex[soundex.Length - 1]))
+        {
+            soundex.Append(code);
+        }
+    }
+
+    private static void FinalizeSoundex(StringBuilder soundex)
+    {
+        while (soundex.Length < 4)
+        {
+            soundex.Append('0');
+        }
+        soundex.Length = 4;
     }
 
     public static string GenerateSoundex(string name)
     {
-        if (string.IsNullOrEmpty(name))
-        {
-            throw new ArgumentException("Name cannot be null or empty.");
-        }
-
         StringBuilder soundex = new StringBuilder();
         soundex.Append(char.ToUpper(name[0]));
 
-        int sIndex = 1;
-        for (int i = 1; i < name.Length && sIndex < 4; i++)
+        for (int i = 1; i < name.Length && soundex.Length < 4; i++)
         {
             char code = GetSoundexCode(name[i]);
-            if (code != '0' && code != soundex[sIndex - 1])
-            {
-                soundex.Append(code);
-                sIndex++;
-            }
+            AppendSoundexCode(code, soundex);
         }
 
-        while (sIndex < 4)
-        {
-            soundex.Append('0');
-            sIndex++;
-        }
-
-        return soundex.ToString().Substring(0, 4);
+        FinalizeSoundex(soundex);
+        return soundex.ToString();
     }
 }
+

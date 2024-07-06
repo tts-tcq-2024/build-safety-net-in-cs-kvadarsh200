b9df88f9d1a04bac0102da6a83a00e15221db4e7
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-06 23:37:50 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+ 
 public class Soundex
 {
     private static readonly Dictionary<char, char> soundexMap = new Dictionary<char, char>
@@ -14,54 +14,59 @@ public class Soundex
         { 'M', '5' }, { 'N', '5' },
         { 'R', '6' }
     };
-
+ 
     public static string GenerateSoundex(string name)
     {
         if (string.IsNullOrEmpty(name))
         {
             return string.Empty;
         }
-
+ 
         StringBuilder soundex = new StringBuilder();
         soundex.Append(char.ToUpper(name[0]));
         char prevCode = GetSoundexCode(name[0]);
-
+ 
         ProcessCharacters(name, soundex, ref prevCode);
-
+ 
         return PadSoundex(soundex).ToString();
     }
-
+ 
     private static void ProcessCharacters(string name, StringBuilder soundex, ref char prevCode)
     {
         for (int i = 1; i < name.Length && soundex.Length < 4; i++)
         {
-            char code = GetSoundexCode(name[i]);
-            if (IsAppendableCode(code, prevCode))
-            {
-                soundex.Append(code);
-                prevCode = code;
-            }
+            AppendSoundexCode(name[i], soundex, ref prevCode);
         }
     }
-
+ 
+    private static void AppendSoundexCode(char c, StringBuilder soundex, ref char prevCode)
+    {
+        char code = GetSoundexCode(c);
+        if (IsAppendableCode(code, prevCode))
+        {
+            soundex.Append(code);
+            prevCode = code;
+        }
+    }
+ 
     private static bool IsAppendableCode(char code, char prevCode)
     {
         return code != '0' && code != prevCode;
     }
-
+ 
     private static StringBuilder PadSoundex(StringBuilder soundex)
     {
         while (soundex.Length < 4)
         {
             soundex.Append('0');
         }
-
+ 
         return soundex;
     }
-
+ 
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
-        return soundexMap.TryGetValue(c, out char code) ? code : '0';
+        return soundexMap.ContainsKey(c) ? soundexMap[c] : '0';
     }
 }
